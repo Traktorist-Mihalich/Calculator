84ae3a34ac0c645081662d4846b7db25f5b8533b
Сергей <152088048+Traktorist-Mihalich@users.noreply.github.com>
2023-11-29 10:28:28 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,6 +4,12 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите числа и оператор");
+        String input = scanner.nextLine();
+        System.out.println(calc(input));
+    }
 
     public static String calc(String input) throws Exception {
         String[] oper = input.split(" ");
